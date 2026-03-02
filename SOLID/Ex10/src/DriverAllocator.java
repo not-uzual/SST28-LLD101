0d4334cd516ed21c31f8298aa0385cd14f2555c3
Ujjwal Sahu <ujjwalsahu19@gmail.com>
2026-03-02 17:53:27 +0530

    refactor(Ex10): refactor transport-booking service following DIP

--- SOLID/Ex10/src/DriverAllocator.java
+++ SOLID/Ex10/src/DriverAllocator.java
@@ -1,4 +1,5 @@
-public class DriverAllocator {
+public class DriverAllocator implements DriverService {
+    @Override
     public String allocate(String studentId) {
         // fake deterministic driver
         return "DRV-17";
